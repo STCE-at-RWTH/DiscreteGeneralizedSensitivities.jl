0bf87e29f72f2b049b838995a327594c47239f36
Sasha Fleming <fleming@stce.rwth-aachen.de>
2025-12-29 14:46:56 +0100

    implementation success?
    if only I could copy formulae down correctly ON THE FIRST TRY

--- src/DiscreteGeneralizedSensitivities.jl
+++ src/DiscreteGeneralizedSensitivities.jl
@@ -6,21 +6,37 @@ using DifferentiationInterface
 using ForwardDiff: ForwardDiff
 using Interpolations: Interpolations, scale, interpolate, BSpline
 
+export DiscreteSensitivityProblemCfg, DiscreteSensitivityBurgers
+export get_initial_conditions
+export cfl_safety_factor, cs_scaling_factor, cr_scaling_factor, alpha
+export nonlinear_f, nonlinear_df, u_exact, u_broad
+export ξ, dξ_dp, jump_size
+
+export GTVClosure
+export solve_pde!, compute_Cr, compute_Cs, compute_Ct
+
 const fdiff_backend = AutoForwardDiff()
 
 """
+    Fixed_t_x{F, T, X}
 
+Fix some callable with signature `f(t, x, p)` at a specific `t` and `x`. 
 """
-struct Fixed_t_x{F,T}
+struct Fixed_t_x{F,T,X}
     fn::F
     t::T
-    x::T
+    x::X
 end
 
-function (fix_tx::Fixed_t_x{F,T})(p) where {F,T}
+function (fix_tx::Fixed_t_x{F,T,X})(p) where {F,T,X}
     return fix_tx.fn(fix_tx.t, fix_tx.x, p)
 end
 
+"""
+    Fixed_t_p{F, T, X}
+
+Fix some callable with signature `f(t, x, p)` at a specific `t` and `p`. 
+"""
 struct Fixed_t_p{F,T,P}
     fn::F
     t::T
@@ -46,7 +62,7 @@ u0_ramp(x, p) = (1 + p) * x * X(x, (zero(x), one(x)))
 
 ξ_ramp(t, p) = sqrt(1 + (1 + p) * t)
 const dξ_ramp_fwdprep = prepare_derivative(Fix1(ξ_ramp, 0.0), fdiff_backend, 0.0)
-dξ_ramp_dp(t, p) = derivative(Fix1(ξ_ramp, t), dξ_ramp_fwdprep, fdiff_backend, p)
+dξ_ramp_dp(t, p) = derivative(Fix1(ξ_ramp, t), fdiff_backend, p)
 
 u_ramp(t, x, p) = ((1 + p) * x) / (1 + (1 + p) * t) * X(x, zero(x), ξ_ramp(t, p))
 Δu_ramp(t, p) = (1 + p) / ξ_ramp(t, p)
@@ -79,6 +95,37 @@ u_broad(::DiscreteSensitivityBurgers) = du_ramp_dp
 dξ_dp(::DiscreteSensitivityBurgers) = dξ_ramp_dp
 
 jump_size(::DiscreteSensitivityBurgers) = Δu_ramp
+
+function get_initial_conditions(xs, p, pdot, cfg::DiscreteSensitivityBurgers)
+    U = map(Fix2(u0(cfg), p), xs)
+    Udot = map(xs) do x
+        first(pushforward(Fix1(u0(cfg), x), fdiff_backend, p, (pdot,)))
+    end
+    Ξ = ξ(cfg)(0.0, p)
+    Ξdot = dξ_dp(cfg)(0.0, p) * pdot
+    return (U, Udot, Ξ, Ξdot)
+end
+
+struct GTVClosure{T,XT}
+    xs::XT
+    cr_dx_alpha::T
+    udot::Vector{T}
+    Δu::T
+    Ξ::T
+    Ξdot::T
+end
+
+function (gtv::GTVClosure{T,XT})(x, pdot) where {T,XT}
+    shock_cut = (gtv.Ξ - gtv.cr_dx_alpha, gtv.Ξ + gtv.cr_dx_alpha)
+    udot_broad = piecewise_constant_interp(gtv.udot, gtv.xs)(x) * !X(x, shock_cut)
+    Ξ_shift = gtv.Ξdot * pdot
+    shock_shift = if Ξ_shift > 0
+        gtv.Δu * X(x, gtv.Ξ, gtv.Ξ + Ξ_shift)
+    else
+        -gtv.Δu * X(x, gtv.Ξ + Ξ_shift, gtv.Ξ)
+    end
+    return udot_broad + shock_shift
+end
 
 function compute_Ct(U, cfg)
     return cfl_safety_factor(cfg) / maximum(abs ∘ nonlinear_df(cfg), U)
@@ -116,9 +163,44 @@ function step_lax_friedrichs!(U_next, U, Δt, xs::AbstractRange, cfg)
     return nothing
 end
 
-function step_shock_location(Ξ, U, xs, Δt, cfg)
+function next_shock_location(Ξ, U, xs, Δt, cfg)
     U_shock = piecewise_constant_interp(U, xs)(Ξ)
     return Ξ + Δt * nonlinear_df(cfg)(U_shock)
+end
+
+function next_shock_sensitivity(Ξ, Ξdot, U, Udot, xs, Δt, Cr, cfg)
+    dx = step(xs)
+    ΞL = Ξ - Cr * dx^alpha(cfg)
+    ΞR = Ξ + Cr * dx^alpha(cfg)
+    @assert(ΞL < ΞR)
+    @assert(Δt > 0)
+
+    U_const = piecewise_constant_interp(U, xs)
+    Udot_const = piecewise_constant_interp(Udot, xs)
+
+    UL = U_const(ΞL)
+    UL_far = U_const(ΞL - 2 * dx)
+    UR = U_const(ΞR)
+    UR_far = U_const(ΞR + 2 * dx)
+    ΔU_shock = UL - UR
+
+    dUdx_L = (UL - UL_far) / (2 * dx)
+    dUdx_R = (UR_far - UR) / (2 * dx)
+
+    UdotL = Udot_const(ΞL)
+    UdotR = Udot_const(ΞR)
+
+    fL = nonlinear_f(cfg)(UL)
+    fR = nonlinear_f(cfg)(UR)
+    dfL = nonlinear_df(cfg)(UL)
+    dfR = nonlinear_df(cfg)(UR)
+
+    A_L = (dfL * (dUdx_L * Ξdot + UdotL)) / ΔU_shock
+    A_R = (dfR * (dUdx_R * Ξdot + UdotR)) / ΔU_shock
+    B = (fL - fR) / (ΔU_shock^2)
+    C_L = (dUdx_L * Ξdot + UdotL)
+    C_R = (dUdx_R * Ξdot + UdotR)
+    return Ξdot + Δt * (A_L - A_R - B * (C_L - C_R))
 end
 
 function solve_pde!(U, xs::AbstractRange, T_end, cfg; recompute_dt = false)
@@ -186,9 +268,20 @@ function solve_pde!(U, Udot, xs::AbstractRange, T_end, cfg; recompute_dt = false
     return (U, Udot)
 end
 
-function solve_pde!(U, Udot, Ξ, xs::AbstractRange, T_end, cfg; recompute_dt = false)
+function solve_pde!(
+    U,
+    Udot,
+    Ξ,
+    Ξdot,
+    xs::AbstractRange,
+    T_end,
+    Cr_0,
+    cfg;
+    recompute_dt = false,
+)
     dx = step(xs)
     dt = compute_Ct(U, cfg) * dx
+    Cr = Cr_0
     t = zero(T_end)
     U_temp = similar(U)
     Udot_temp = similar(Udot)
@@ -211,7 +304,11 @@ function solve_pde!(U, Udot, Ξ, xs::AbstractRange, T_end, cfg; recompute_dt = f
             dt = T_end - t
             stepping = false
         end
-        Ξ = step_shock_location(Ξ, U, xs, dt, cfg)
+        if dt < 0
+            break
+        end
+        Ξdot = next_shock_sensitivity(Ξ, Ξdot, U, Udot, xs, dt, Cr, cfg)
+        Ξ = next_shock_location(Ξ, U, xs, dt, cfg)
         value_and_pushforward!(
             step_lax_friedrichs!,
             U_temp,
@@ -228,15 +325,21 @@ function solve_pde!(U, Udot, Ξ, xs::AbstractRange, T_end, cfg; recompute_dt = f
         U .= U_temp
         Udot .= Udot_temp
     end
-    return (U, Udot, Ξ)
+    return (U, Udot, Ξ, Ξdot)
 end
 
 # Characteristic function of an interval [a, b]
 X(x, a, b) = a ≤ x ≤ b
 X(x, ival) = X(x, ival...)
 
-function piecewise_constant_interp(data, xs)
-    return scale(interpolate(data, BSpline(Interpolations.Constant())), xs)
+function piecewise_constant_interp(data, xs::AbstractRange)
+    # I am too lazy to write a functor. closure time.
+    return (x,) -> let data = data, xs = xs
+        x <= first(xs) && return first(data)
+        x >= last(xs) && return last(data)
+        offset = round(Int, (x - first(xs)) / step(xs))
+        return data[begin+offset]
+    end
 end
 
 end
